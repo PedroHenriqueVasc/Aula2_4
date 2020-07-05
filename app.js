2324c32ef5272025bd2ab0200749c860740c36d8
pedro.henrique <pedro.new19@gmail.com>
2020-07-04 21:02:20 -0300

    variaveis de ambiente encapsuladas com dotenv

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { studentRouter } from './routes/studentRouter.js';
 
-//Conexão com o Banco
-(async () => {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://igtiUserDb:userpassword@cluster0.514id.mongodb.net/grades?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-  } catch (err) {
-    console.log('Erro ao conectar ao Mongo DB');
+require('dotenv').config()(
+  //Conexão com o Banco
+  async () => {
+    try {
+      await mongoose.connect(
+        `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.514id.mongodb.net/grades?retryWrites=true&w=majority`,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      );
+    } catch (err) {
+      console.log('Erro ao conectar ao Mongo DB');
+    }
   }
-})();
+)();
 
 //Inicialização dda API
 const app = express();
@@ -24,4 +26,4 @@ const app = express();
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3000, () => console.log('API iniciada'));
+app.listen(process.env.PORT, () => console.log('API iniciada'));
